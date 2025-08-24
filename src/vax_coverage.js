0c930879a8f186b025005b528f13bffe51fd1adc
Joe Schueller <hello@joeschueller.com>
2025-08-24 17:03:21 -0500

    lots more updates

--- src/vax_coverage.js
+++ src/vax_coverage.js
@@ -2,13 +2,42 @@ import * as Plot from "npm:@observablehq/plot";
 import { FileAttachment } from "observablehq:stdlib";
 
 export async function renderChart() {
+
   const data = await FileAttachment("./data/vax_coverage.csv").csv({ typed: true });
+
   return Plot.plot({
     width: 680,
-    height: 450,
+    height: 420,
+    x: {
+      label: "",
+      tickRotate: 0,
+      axis: "bottom",
+      grid: false
+    },
+    y: {
+      label: "Estimated coverage (%)",
+      domain: [88.0, 100.0],
+      axis: "left",
+      grid: true
+    },
     marks: [
-        Plot.lineY(data, {x: "school_year", y: "estimate_pct"}),
-        Plot.text(data, {x: "school_year", y: "estimate_pct", text: "vaccine_exemption", lineAnchor: "bottom", dy: -10, lineWidth: 10, fontSize: 12})
+      Plot.line(data, {
+        x: "school_year",
+        y: "estimate_pct",
+        stroke: "#d1221f",
+        strokeWidth: 2.5,
+        curve: "natural",
+        marker: "circle",
+        tip: {
+          channels: {
+            "School Year: ": d => d.school_year,
+            "Estimated coverage (%): ": d => d.estimate_pct.toFixed(1) + "%"
+          },
+          format: { x: null, y: null }
+        }
+      }),
+      Plot.frame({ anchor: "bottom", stroke: "black", strokeWidth: 1.25 }),
     ]
   });
+  
 }
--- src/vax_coverage.md
+++ src/vax_coverage.md
@@ -1,6 +1,9 @@
 ---
 title: vax_coverage
 ---
+<html>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source%20Sans%20Pro">
+</html>
 
 ```js
 import {renderChart} from "./vax_coverage.js";
